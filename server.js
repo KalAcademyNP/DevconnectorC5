1f178b488178e10f89199d05c4f9c76f9b55fd40
Kal <info@kalacademy.org>
2019-09-08 11:32:50 -0700

    Routes

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 //Db config
@@ -12,5 +16,11 @@ mongoose
 
 //First route
 app.get("/", (req, res) => res.send("hello!"));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
 const port = 7000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
