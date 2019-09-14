c8743ee1d9841efa9c0b48ecd0c80a10903b8ddb
Kal <info@kalacademy.org>
2019-09-14 11:19:31 -0700

    User model and api

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
 
-router.get('/test', (req, res) => res.json({
-  msg: 'User works!'
-}));
+// @route   POST api/users/register
+// @desc    Register a user
+// @access  Public
+router.post('/register', (req, res) => {
+  User.findOne({email: req.body.email})
+    .then(user => {
+      if (user) {
+        return res.status(400).json({
+          email: 'Email already exists'
+        })
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        });
+      }
+    })
+    .catch(err => console.log(err));
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //Db config
 const db = require('./config/keys').mongoURI;
