96b04a92889d21499e3a30ebb22a2885dce0b373
Kal <info@kalacademy.org>
2019-09-21 11:26:04 -0700

    Passport authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,24 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../../models/User');
-const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs');
+const User = require("../../models/User");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require('passport');
 
 // @route   POST api/users/register
 // @desc    Register a user
 // @access  Public
-router.post('/register', (req, res) => {
-  User.findOne({email: req.body.email})
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email })
     .then(user => {
       if (user) {
         return res.status(400).json({
-          email: 'Email already exists'
-        })
+          email: "Email already exists"
+        });
       } else {
         const avatar = gravatar.url(req.body.email, {
-          s: '200',
-          r: 'pg',
-          d: 'mm'
+          s: "200",
+          r: "pg",
+          d: "mm"
         });
         const newUser = new User({
           name: req.body.name,
@@ -32,34 +35,72 @@ router.post('/register', (req, res) => {
           bcrypt.hash(newUser.password, salt, (err, hash) => {
             if (err) throw err;
             newUser.password = hash;
-            newUser.save()
+            newUser
+              .save()
               .then(user => res.json(user))
               .catch(err => console.log(err));
-          })
-        })
-
-      }
-    })
-    .catch(err => console.log(err));
-})
-
-// @route   POST api/users/login
-// @desc    Login user / return JWT token
-// @access  Public
-router.post('/login', (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-
-  //Find user by email
-  User.findOne({email})
-    .then(user => {
-      if (!user){
-        return res.status(404).json({
-          email: 'User not found'
+          });
         });
       }
     })
     .catch(err => console.log(err));
-})
+});
+
+// @route   POST api/users/login
+// @desc    Login user / return JWT token
+// @access  Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user by email
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({
+          email: "User not found"
+        });
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then(isMatch => {
+          if (!isMatch) {
+            return res.status(400).json({
+              password: "Password does not match."
+            });
+          }
+          const payload = {
+            id: user.id,
+            name: user.name,
+            avatar: user.avatar
+          };
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+              if (err) throw err;
+
+              return res.json({
+                success: true,
+                token: "Bearer " + token
+              });
+            }
+          );
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+});
+
+// @route   GET api/users/current
+// @desc    return current user
+// @access  Private
+router.get(
+  '/current', 
+  passport.authenticate('jwt', {session: false}),
+  (req, res) => {
+    res.json({msg: 'Success'});
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const app = express();
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+//Passport configuration
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 //Db config
 const db = require('./config/keys').mongoURI;
